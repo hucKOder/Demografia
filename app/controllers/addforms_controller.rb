4b2c4d27bbd8130a5eef51380a798e81b1668ff3
Andrej <hucko.andrej.mail@gmail.com>
2017-04-23 21:38:57 +0200

    Hotove pridavanie zaznamov

--- app/controllers/addforms_controller.rb
+++ app/controllers/addforms_controller.rb
@@ -15,12 +15,19 @@ class AddformsController < ApplicationController
       exec(transaction, "Úspešne pridaný záznam o narodeniach v meste #{params[:town]}")
 
     elsif params[:commit] == "Pridaj záznam o rozvodoch" && params.values_at(:year, :women_age, :man_age, :count, :duration, :gender_petition, :verdict, :town).all?(&:present?)
+      transaction = "BEGIN; INSERT INTO divorces (year, women_age, man_age, count, duration, gender_file_a_petition, verdict, created_at, updated_at, town_id)
+      VALUES(#{params[:year]}, #{params[:women_age]}, #{params[:man_age]}, #{params[:count]}, #{params[:duration]}, '#{params[:gender_petition]}', '#{params[:verdict]}', '#{time}', '#{time}', #{town_id}); COMMIT;"
+      exec(transaction, "Úspešne pridaný záznam o rozvodoch v meste #{params[:town]}")
 
+    elsif params[:commit] == "Pridaj záznam o migráciách"  && params.values_at(:year, :age, :emigrants, :imigrants, :gender, :reason, :town).all?(&:present?)
+      transaction = "BEGIN; INSERT INTO migrations (year, age, emigrants, imigrants, gender, reason, town_id, updated_at, created_at)
+      VALUES(#{params[:year]}, #{params[:age]}, #{params[:emigrants]}, #{params[:imigrants]}, '#{params[:gender]}', '#{params[:reason]}', #{town_id}, '#{time}', '#{time}'); COMMIT;"
+      exec(transaction, "Úspešne pridaný záznam o migráciách v meste #{params[:town]}")
 
-    elsif params[:commit] == "Pridaj záznam o migráciách" && params.values_at(:year, :bride_age, :fiance_age, :count, :number, :town).all?(&:present?)
-
-
-    elsif params[:commit] == "Pridaj záznam o sobášoch" && params.values_at(:year, :age, :emigrants, :imigrants, :gender, :reason, :town).all?(&:present?)
+    elsif params[:commit] == "Pridaj záznam o sobášoch" && params.values_at(:year, :bride_age, :fiance_age, :count, :number, :town).all?(&:present?)
+      transaction = "BEGIN; INSERT INTO mariages (year, bride_age, fiance_age, count, mariage_count, town_id, updated_at, created_at)
+      VALUES(#{params[:year]}, #{params[:bride_age]}, #{params[:fiance_age]}, #{params[:count]}, '#{params[:number]}', #{town_id}, '#{time}', '#{time}'); COMMIT;"
+      exec(transaction, "Úspešne pridaný záznam o sobášoch v meste #{params[:town]}")
 
     else
       redirect_to addforms_path, flash: { notice: "Neboli vyplnené všetky polia! Záznam nebol pridaný!"}
---
+++ app/controllers/edits_controller.rb
@@ -0,0 +1,9 @@
+class EditsController < ApplicationController
+  def index
+    @x = params[:items]
+  end
+
+  def delete
+
+  end
+end
