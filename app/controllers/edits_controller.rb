6348c00fd0efc8696b0c5a9f84c88eca03aa92ea
Andrej <hucko.andrej.mail@gmail.com>
2017-04-27 17:09:13 +0200

    Pridane validates do modelov

--- app/controllers/edits_controller.rb
+++ app/controllers/edits_controller.rb
@@ -9,4 +9,32 @@ class EditsController < ApplicationController
     ActiveRecord::Base.connection.execute(sql)
   end
 
+  def edit
+    sql = "BEGIN; UPDATE #{params[:type]}
+           SET
+              #{get_set(params)}
+           WHERE
+              id = #{params[:id]};
+           COMMIT;"
+
+    ActiveRecord::Base.connection.execute(sql)
+  end
+
+  #town cannot be updated
+  def get_set(params)
+    sql = ""
+    params.stringify_keys.each do |key, value|
+      if ActiveRecord::Base.connection.column_exists?(params[:type].parameterize.underscore.to_sym, key.parameterize.underscore.to_sym)
+        if value.present? then
+          if key == "gender" then
+            sql += "#{key} = '#{value}', "
+          else
+            sql += "#{key} = #{value}, "
+          end
+        end
+      end
+    end
+
+    sql[0..-3]
+  end
 end
--- app/models/death.rb
+++ app/models/death.rb
@@ -1,3 +1,11 @@
 class Death < ApplicationRecord
 	belongs_to :town
+
+	validates :year, :numericality => { :only_integer => true, message: "Rok môže byť iba celé číslo"}
+  validates :age, :numericality => { :only_integer => true, message: "Vek môže byť iba celé číslo"}
+  validates :count, :numericality => { :only_integer => true, message: "Počet môže byť iba celé číslo"}
+  validates :gender, :acceptance => { :accept => ['Female', 'Male'], message: "Pohlavie môže byť iba Male alebo Female" }
+  validates :town_id, presence: true
+
+  validates_uniqueness_of :year, { :scope => [:town_id, :age, :gender], message: "Záznam v tohoto roku, veku a pohlavia už existuje!" }
 end
