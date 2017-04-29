16a2d3adda4bea717eaa15261bd18702a789ffc7
Andrej <hucko.andrej.mail@gmail.com>
2017-04-29 17:35:59 +0200

    Pridane grafy

--- app/controllers/migrations_controller.rb
+++ app/controllers/migrations_controller.rb
@@ -4,9 +4,31 @@ class MigrationsController < ApplicationController
 		@migrations = Migration.find_by_sql("SELECT * FROM #{params[:type]} WHERE town_id = #{params[:id]}")
 		@type = params[:type]
 
-    info = "SELECT year, sum(count) x FROM deaths
+    info = "SELECT year y, sum(count) x FROM #{params[:type]}
     WHERE town_id = #{params[:id]}
     GROUP BY year
     ORDER BY year ASC"
+    infoMigration = "SELECT year y, sum(emigrants) x, sum(imigrants) i FROM #{params[:type]}
+              WHERE town_id = #{params[:id]}
+              GROUP BY year
+              ORDER BY year ASC"
+    @information = get_type(params, info, infoMigration)
+
+	end
+
+	def get_type(params, info, infoMigration)
+		record = ""
+    case params[:type]
+			when "deaths"
+				record = Death.find_by_sql(info)
+			when "births"
+				record = Birth.find_by_sql(info)
+			when "mariages"
+				record = Mariage.find_by_sql(info)
+			when "divorces"
+				record = Divorce.find_by_sql(info)
+			when "migrations"
+				record = Migration.find_by_sql(infoMigration)
+		end
 	end
 end
