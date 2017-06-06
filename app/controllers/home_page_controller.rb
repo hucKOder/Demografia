852896d037f00da735d949e1930bcfcfca8253ea
Andrej <hucko.andrej.mail@gmail.com>
2017-06-06 18:37:14 +0200

    Prvy test heroku

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,5 +1,9 @@
 class HomePageController < ApplicationController
   def index
-  	
+  	@divorces = Divorce.find_by_sql("SELECT year, sum(divorces_number) d FROM divorces GROUP BY year")
+    @marriages = Marriage.find_by_sql("Select year, sum(male_home_number) m, sum(female_home_number) f
+                                       FROM marriages GROUP BY year")
+    @births = Birth.find_by_sql("Select year,(sum(girls) + sum(boys)) sum from births GROUP BY year")
+    @deaths = Death.find_by_sql("select year, (sum(male) + sum(female)) sum from deaths GROUP BY year")
   end
 end
--- test/controllers/addforms_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AddformsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
