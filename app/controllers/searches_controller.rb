46bbd3e5d363abcbfb3d7db953cc27cdb27a84f3
Andrej <hucko.andrej.mail@gmail.com>
2017-06-17 17:13:50 +0200

    Rebricek miest, kazde mesto ma porovnanie s priemerom

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -30,10 +30,48 @@ join (select year, sum(male_imigrants) + sum(female_imigrants) plus, sum(male_em
 	group by births.year, x.d,  y.plus, y.min
 	order by births.year) res")
 
-		@bd_diff = @bd_diff.map{ |i| [i.year, i.result]}
-		@bd_diff.insert(0,["Rok", "Nárast obyvateľstva"])
+		@bd_diff = @bd_diff.map{ |i| [i.year, (i.result.to_f/@town.citizens.to_f * 100).round(3)]}
 
-		@md_diff
+
+		@avg = Birth.find_by_sql("select births.year, (sum(boys) + sum(girls)) - d.sum + m.sum sum
+from births
+
+join (select year, sum(male) + sum(female) sum
+	from deaths
+	group by year) d on d.year = births.year
+
+join (select year, sum(male_imigrants) + sum(female_imigrants) - sum(male_emigrants) - sum(female_emigrants) sum
+	from migrations
+	group by year) m on m.year = births.year
+
+group by births.year, d.sum, m.sum
+order by year")
+
+
+		@population = Town.average(:citizens)
+		@avg = @avg.map { |m| [m.year, m.sum.to_f/(138*@town.citizens).to_f*100] }
+
+		@bd_diff = @bd_diff.map.with_index { |m,i| [m.first, m.second, @avg[i].second]}
+		@bd_diff.insert(0,["Rok", "Nárast obyvateľstva v %", "Priemer Slovenska zhľadom na počet obyvateľov mesta"])
+
+			@rank = Birth.find_by_sql("select rank() OVER (order by (sum(boys) + sum(girls) - d.sum + m.sum)/t.citizens::float *100 DESC), births.town_id, t.name,
+																 (sum(boys) + sum(girls) - d.sum + m.sum)/t.citizens::float * 100 sum, t.citizens
+			from births
+
+			join (select sum(male) + sum(female) sum, town_id
+			from deaths
+			where year = 2015
+			group by town_id) d on d.town_id = births.town_id
+
+			join (select sum(male_imigrants) + sum(female_imigrants) - sum(male_emigrants) - sum(female_emigrants) sum, town_id
+			from migrations
+			where year = 2015
+			group by town_id) m on m.town_id= births.town_id
+			join (select id, citizens, name from towns) t on t.id = births.town_id
+			where births.year = 2015
+			group by d.sum, m.sum, births.town_id, t.citizens, t.name")
+
+		@rank = @rank.map { |i| [i.town_id,[i.rank, i.name, i.sum, i.citizens]] }.to_h
 	end
 
 end
--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -1,5 +1,24 @@
 class YearsController < ApplicationController
 	def index
+		@rank = Birth.find_by_sql("select rank() OVER (order by (sum(boys) + sum(girls) - d.sum + m.sum)/t.citizens::float *100 DESC), births.town_id, t.name,
+																 (sum(boys) + sum(girls) - d.sum + m.sum)/t.citizens::float * 100 sum, t.citizens
+			from births
+
+			join (select sum(male) + sum(female) sum, town_id
+			from deaths
+			where year = #{params["Rok"]["year"]}
+			group by town_id) d on d.town_id = births.town_id
+
+			join (select sum(male_imigrants) + sum(female_imigrants) - sum(male_emigrants) - sum(female_emigrants) sum, town_id
+			from migrations
+			where year = #{params["Rok"]["year"]}
+			group by town_id) m on m.town_id= births.town_id
+			join (select id, citizens, name from towns) t on t.id = births.town_id
+			where births.year = #{params["Rok"]["year"]}
+			group by d.sum, m.sum, births.town_id, t.citizens, t.name")
+
+		@rank = @rank.map { |i| [i.town_id,[i.rank, i.name, i.sum, i.citizens]] }.to_h
+
 
 	end
 end
