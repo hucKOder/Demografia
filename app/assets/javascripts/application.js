ab303db9ea06498213343f681441ffebff78cdfd
Andrej <hucko.andrej.mail@gmail.com>
2017-06-18 15:03:37 +0200

    Dokoncenie tabuliek s poradim, Pridanie analytik

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,4 @@
 //= require d3
 //= require bootstrap-sprockets
 //= require_tree .
+
--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -19,6 +19,19 @@ class YearsController < ApplicationController
 
 		@rank = @rank.map { |i| [i.town_id,[i.rank, i.name, i.sum, i.citizens]] }.to_h
 
+		@births = Birth.find_by_sql("Select rank() over (order by (sum(boys) + sum(girls))/citizens::float*100 DESC), citizens,
+  (sum(boys) + sum(girls))/citizens::float*100 sum, town_id, name
+  from births
+  join towns on town_id = towns.id
+  where year = #{params["Rok"]["year"]}
+  group by town_id, towns.name ,towns.citizens")
+
+    @deaths = Death.find_by_sql("Select rank() over (order by (sum(male) + sum(female))/citizens::float*100 DESC),
+(sum(female) + sum(male))/citizens::float*100 sum, town_id, name, citizens
+from deaths
+join towns on town_id = towns.id
+where year = #{params["Rok"]["year"]}
+group by town_id, towns.name ,towns.citizens")
 
 	end
 end
